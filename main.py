f9be998d585bd8915fab552efcd067d30f892eb1
missketchum14 <vickyw95@hotmail.com>
2017-01-28 17:58:36 -0500

    texting only

--- main.py
+++ main.py
@@ -8,17 +8,6 @@ client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
 message = client.messages.create(
     to = '+16138699318', #receiver
     from_ = '+13437006039', #customer
-    body = 'Hi friend, send me your symptoms separated by spaces\n',
+    body = 'Hi friend',
 )
 print (message.sid)
-
-
-#user inputs question
-def get_symptoms():
-    '''
-    None -> None
-    '''
-    symptoms = input("Send me your symptoms separated by spaces\n")
-
-
-print ("Hello World")
