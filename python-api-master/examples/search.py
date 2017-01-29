86109d02f835cf45ae7dd7e393571ce10d2e9b0d
maximedaigle <maximedaigle95@gmail.com>
2017-01-28 21:15:58 -0500

    first working symptoms checker

--- python-api-master/examples/search.py
+++ python-api-master/examples/search.py
@@ -10,14 +10,17 @@ if __name__ == '__main__':
     api = infermedica_api.get_api()
 
     print('Look for evidences containing phrase headache:')
-    print(api.search('headache'), end="\n\n")
-
-    print('Look for evidences containing phrase breast, only for female specific symptoms:')
-    print(api.search('breast', sex='female'), end="\n\n")
-
-    print('Look for evidences containing phrase breast, only for female specific symptoms, with the limit of 5 results:')
-    print(api.search('breast', sex='female', max_results=5), end="\n\n")
-
-    print('Look for symptoms and risk factors containing phrase trauma:')
-    print(api.search('trauma', filters=[
-        infermedica_api.SEARCH_FILTERS.SYMPTOMS, infermedica_api.SEARCH_FILTERS.RISK_FACTORS]), end="\n\n")
+    test = api.search('headache')
+    print("helloooooo\n")
+    print(test[0]["id"])
+    #print(api.search('headache'), end="\n\n")
+    #
+    # print('Look for evidences containing phrase breast, only for female specific symptoms:')
+    # print(api.search('breast', sex='female'), end="\n\n")
+    #
+    # print('Look for evidences containing phrase breast, only for female specific symptoms, with the limit of 5 results:')
+    # print(api.search('breast', sex='female', max_results=5), end="\n\n")
+    #
+    # print('Look for symptoms and risk factors containing phrase trauma:')
+    # print(api.search('trauma', filters=[
+    #     infermedica_api.SEARCH_FILTERS.SYMPTOMS, infermedica_api.SEARCH_FILTERS.RISK_FACTORS]), end="\n\n")
