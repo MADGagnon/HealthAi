158d1b9ed9b358d9eb46904e6998fdef6455f596
MADGagnon <mdufresnegagnon@gmail.com>
2017-01-28 19:10:10 -0500

    localserver

---
+++ run.py
@@ -0,0 +1,20 @@
+# from flask import Flask, request, redirect
+# import twilio.twiml
+# import math
+
+
+"""
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def hello_monkey():
+
+    resp = twilio.twiml.Response()
+    resp.message("Hello,"+(math.sin(math.pi/2))+"lol")
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+"""
+print("hey")
